bdb7e045d553f972a29ba0855fe697f85f220519
Susan <susanschrum@mgail.com>
2015-12-19 00:05:42 -0500

    Fixed back to work ref

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -27,29 +27,33 @@ get_header(); ?>
 			?>
 
 			<section class="client-info">
-		
-			<aside class="cpost-entry">
-						
+
+				<aside class="cpost-entry">
+
 					<h1><?php echo the_title(); ?></h1>
 					<h4><?php echo $services; ?></h4>
 					<h2>Client: <?php echo $client; ?></h2>
 					<p><?php the_content(); ?></p>
 					<a href="<?php $link ?>"><strong>Visit Live Site</strong></a>
-				
-			</aside>
 
-			<section class="cpost-pic">
-			 	<ul class="cpost-list">
-					<?php if ($image1) ?>
-					<li><?php echo wp_get_attachment_image($image1,$size); ?></li>
-					<li><?php echo wp_get_attachment_image($image2,$size); ?></li>
-					<li><?php echo wp_get_attachment_image($image3,$size); ?></li>
-				</ul>
+				</aside>
+
+				<section class="cpost-pic">
+					<ul class="cpost-list">
+						<?php if ($image1) ?>
+						<li><?php echo wp_get_attachment_image($image1,$size); ?></li>
+						<li><?php echo wp_get_attachment_image($image2,$size); ?></li>
+						<li><?php echo wp_get_attachment_image($image3,$size); ?></li>
+					</ul>
+				</section>
+
+<div class='clear'></div>
+			
+			<section class="cback-nav">
+				<div class="left">&larr;<a href="http://localhost:8888/accelerate/case-studies/">back to work</a></div>
 			</section>
-
-
-		</section>
-
+			</section>
+			
 
 		</div>
 	</div>
@@ -57,13 +61,12 @@ get_header(); ?>
 
 
 
-	<!-- END blog page -->
 
-	<footer class="navigation main-cs-content">
-		<div class="left">&larr;<a href="">back to work</a></div>
-	</footer>
-	
-				<?php endwhile; ?>
+<!-- END blog page -->
+
+
+
+<?php endwhile; ?>
 
 <?php
 get_footer();
